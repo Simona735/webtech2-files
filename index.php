be8c90cacdb59a617d939493a26a3ed71930333d
Simona735 <72383935+Simona735@users.noreply.github.com>
2021-02-28 00:48:08 +0100

    styling done, link to files done, directory icons done, directory links missing

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Richterova">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" type="text/css" href="css/default.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -18,7 +18,7 @@
         <div class="py-5 text-center">
             <h2>View & upload files</h2>
             <p class="lead">Nižšie môžete nahrať akýkoľvek súbor.</p>
-            <button type="button" onclick="window.location.href='upload.html';" class="btn btn-primary my-2">Nahrať súbor</button>
+            <button type="button" onclick="window.location.href='selectToUpload.php';" class="btn btn-primary my-2">Nahrať súbor</button>
         </div>
     </main>
 
@@ -30,30 +30,56 @@
             <th scope="col" data-sortable="true">Dátum</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody id="tableBody">
         <?php
-            $a = scandir("../files/");
-            foreach ($a as $file){
-                echo "<tr>
-                        <td>{$file}</td>
-                        <td>". (filesize("../files/" . $file)) >>10 . " kB" ."</td>
-                        <td>". date ("F d Y H:i:s", filemtime ("../files/" . $file)) ."</td>
-                       </tr>";
+        $BASE = "../files/";
+
+        $currentpath = "";
+
+        $a = array_diff(scandir($BASE . $currentpath), array('.'));
+        foreach ($a as $file){
+            $path = $BASE . $currentpath . $file;
+            if (!strcmp($file, '..')){
+                if (strcmp($path, $BASE . "..")){
+                    echo "<tr>
+                    <td><i class='bi bi-folder-symlink'></i>&nbsp;
+                        <a href='$path'>parent folder</a>
+                    </td>
+                    <td></td>
+                    <td></td>
+                   </tr>";
+                }
             }
+            else if (is_file($path)){
+                echo "<tr>
+                    <td>
+                        <a href='$path'>{$file}</a>
+                    </td>
+                    <td>" . (filesize($path)) >> 10 . " kB" . "</td>
+                    <td>" . date("F d Y H:i:s", filemtime($path)) . "</td>
+                   </tr>";
+            }else{
+                echo "<tr>
+                    <td><i class='bi bi-folder'></i>&nbsp;{$file}</td>
+                    <td></td>
+                    <td></td>
+                   </tr>";
+            }//<a href="?path=../files/filee">filee</a> for one dir
+        }
         ?>
         </tbody>
     </table>
 
     <footer class="my-3 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy;2021 WEBTECH2 - Richterová</p>
+        <p class="mb-1">&copy;2021 WEBTECH2 - Richterová </p>
     </footer>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.js"  crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <script src="js/sort.js"></script>
+<script src="js/javascript.js"></script>
 </body>
 </html>
--- js/fileUpload.js
+++ js/fileUpload.js
@@ -1,9 +1,14 @@
 // File Upload
 //
 const button = document.querySelector('#submit');
+var isGood;
+
+
+
 
 function ekUpload(){
     function Init() {
+        isGood = 0;
         console.log("init");
         var fileSelect    = document.getElementById('fileToUpload'),
             fileDrag      = document.getElementById('file-drag');
@@ -21,21 +26,28 @@ function ekUpload(){
 
 
         button.addEventListener('click', () => {
-            const form = new FormData(document.querySelector('#file-upload-form'));
-            const url = '/zadanie_01/upload.php'
-            const request = new Request(url, {
-                method: 'POST',
-                body: form
-            });
+            if(isGood){
+                const form = new FormData(document.querySelector('#file-upload-form'));
+                const url = '/zadanie_01/upload.php'
+                const request = new Request(url, {
+                    method: 'POST',
+                    body: form
+                });
 
-            fetch(request)
-                .then(response => response.json())
-                .then(data => { console.log(data); })
+                fetch(request)
+                    .then(response => response.json())
+                    .then(data => { console.log(data); })
+
+                isGood = 0;
+                output("");
+            }else{
+                output("Please select a file");
+                document.getElementById('file-drag').style.border = "3px solid crimson";
+            }
         });
     }
 
     function fileDragHover(e) {
-        console.log("option1");
         var fileDrag = document.getElementById('file-drag');
 
         e.stopPropagation();
@@ -45,26 +57,46 @@ function ekUpload(){
     }
 
     function fileSelectHandler(e) {
-        console.log("option1");
+        document.getElementById('file-drag').style.border = "3px solid #eee";
         // Fetch FileList object
         var files = e.target.files || e.dataTransfer.files;
 
         // Cancel event and hover styling
         fileDragHover(e);
 
+        if(files.length == 0){
+            isGood = 0;
+            output("Please select a file");
+            document.getElementById('file-drag').style.border = "3px solid crimson";
+            return;
+        }
+
         for (var i = 0, f; f = files[i]; i++) {
             parseFile(f);
         }
     }
 
+    // Output
+    function output(msg) {
+        // Response
+        var m = document.getElementById('messages');
+        m.innerHTML = msg;
+        document.getElementById('response').classList.remove("d-none");
+    }
+
     function parseFile(file) {
+        if (file.size >= 2000000 ) {
+            isGood = 0;
+            output("Only files with max size 2MB");
+            document.getElementById('file-drag').style.border = "3px solid crimson";
+            return;
+        }
 
         console.log(file.name);
-        var m = document.getElementById('messages');
-        m.innerHTML =  encodeURI(file.name) ;
-
+        output(encodeURI(file.name));
+        isGood = 1;
+        document.getElementById('file-drag').style.border = "3px solid #eee";
         document.getElementById('start').classList.add("d-none");
-        document.getElementById('response').classList.remove("d-none");
     }
 
     // Check for the various File API support.
