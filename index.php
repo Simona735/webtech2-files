922ae1c7bb57a9c59b9c15e2c4e6b63d97c634db
Simona735 <72383935+Simona735@users.noreply.github.com>
2021-03-01 13:07:23 +0100

    redundant things removed

--- index.php
+++ index.php
@@ -86,11 +86,9 @@
     </footer>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.js" crossorigin="anonymous"></script>
-<!--<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
 <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <script src="js/sort.js"></script>
-<script src="js/javascript.js"></script>
 </body>
 </html>
